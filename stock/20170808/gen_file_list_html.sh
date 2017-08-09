aee43adcf66100d762b9eb47cc93ac6bd9c47641
lin <lin@localhost.localdomain>
2017-08-09 23:35:53 +0800

    v1

--- stock/20170808/gen_file_list_html.sh
+++ stock/20170808/gen_file_list_html.sh
@@ -12,7 +12,8 @@ echo "<UL>" > $OUTPUT
   echo "  <UL>" >> $OUTPUT
   for i in `find "$filepath" -maxdepth 1 -mindepth 1 -type f| sort`; do
     file=`basename "$i"`
-    echo "    <LI><a href=\"/$path/$file\">$file</a></LI>" >> $OUTPUT
+    echo "    <LI><a href=\"$file\">$file</a></LI>" >> $OUTPUT
+    #echo "    <LI><a href=\"/$path/$file\">$file</a></LI>" >> $OUTPUT
   done
   echo "  </UL>" >> $OUTPUT
 #done
