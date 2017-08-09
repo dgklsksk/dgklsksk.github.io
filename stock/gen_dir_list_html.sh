1316c259729dbbda1f3f67816274bef2f6a9358b
lin <lin@localhost.localdomain>
2017-08-09 11:36:03 +0800

    v1

---
+++ stock/gen_dir_list_html.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ROOT=./
+HTTP="/"
+OUTPUT="site-index.html" 
+
+i=0
+echo "<UL>" > $OUTPUT
+filepath=./
+#for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d| sort`; do
+#  path=`basename "$filepath"`
+#  echo "  <LI>$path</LI>" >> $OUTPUT
+  echo "  <UL>" >> $OUTPUT
+  for i in `find "$filepath" -maxdepth 1 -mindepth 1 -type d| sort`; do
+    file=`basename "$i"`
+    echo "    <LI><a href=\"/$path/$file/site-index.html\">$file</a></LI>" >> $OUTPUT
+  done
+  echo "  </UL>" >> $OUTPUT
+#done
+echo "</UL>" >> $OUTPUT
